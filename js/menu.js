ead6a602b63ba67241e387843a83373d03cef1ef
aerix <vetalaerix@gmail.com>
2015-04-17 13:01:34 +0300

    Included some icons for some main menu items.

--- js/menu.js
+++ js/menu.js
@@ -74,14 +74,14 @@ if (document.body.clientWidth <= 600)
 	element.className += " hidden";
 }
 
-menu.items.push(new Item("Start", "#!start/", "img/menu/dashboard.png"));
-menu.items.push(new Item("Expenses", "#!expenses/", "img/menu/expenses.png"));
+menu.items.push(new Item("Start", "#!start/", "img/menu/vector_65_03-64.png"));
+menu.items.push(new Item("Expenses", "#!expenses/", "img/menu/vector_65_04-64.png"));
 menu.items.push(new Item("Expense types", "#!expenses/types/", ""));
-menu.items.push(new Item("Profits", "#!page1/", ""));
+menu.items.push(new Item("Profits", "#!page1/", "img/menu/vector_65_05-64.png"));
 menu.items.push(new Item("Profit types", "#!page2/", ""));
 menu.items.push(new Item("Tables", "#!tables/", ""));
 menu.items.push(new Item("Excel", "#!excel/", ""));
-menu.items.push(new Item("User", "/", ""));
+menu.items.push(new Item("User", "/", "img/menu/vector_65_01-64.png"));
 
 menu.render("items");
 
