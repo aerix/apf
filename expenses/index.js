3f12c5e6daf850f93664084cb9d4746955b49177
aerix <vetalaerix@gmail.com>
2015-05-15 18:36:56 +0300

    Draft of contols mechanism.

--- expenses/index.js
+++ expenses/index.js
@@ -1,28 +1,3 @@
-This.Period = function()
-{
-	var now = new Date();
-
-	var startDate = new Date(now.getFullYear(), now.getMonth(), 1);
-	var endDate = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-
-	this.start = This.date2Str(startDate);
-	this.end = This.date2Str(endDate);
-};
-
-
-This.date2Str = function(date)
-{
-	return date.getFullYear() + 
-		"-" + 
-		("0" + (date.getMonth() + 1)).slice(-2) +
-		"-" +
-		("0" + date.getDate()).slice(-2);
-};
-
-This.period = new This.Period();
-document.getElementById("periodStart").value = This.period.start;
-document.getElementById("periodEnd").value = This.period.end;
-
 This.insert = function()
 {
 	var day = prompt("Day", This.date2Str(new Date()));
@@ -71,4 +46,9 @@ This.select = function()
 		 });
 };
 
-This.select();
+This.afterInitPeriod = function()
+{
+	This.select();
+};
+
+exec("controls/period/init.js", This.afterInitPeriod);
--- start/js/index.js
+++ start/js/index.js
@@ -1,25 +1,7 @@
-This.Period = function()
+This.afterInitPeriod = function()
 {
-	var now = new Date();
-
-	var startDate = new Date(now.getFullYear(), now.getMonth(), 1);
-	var endDate = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-
-	this.start = This.date2Str(startDate);
-	this.end = This.date2Str(endDate);
+	// console.log("afterInitPeriod");
 };
 
-
-This.date2Str = function(date)
-{
-	return date.getFullYear() + 
-		"-" + 
-		("0" + (date.getMonth() + 1)).slice(-2) +
-		"-" +
-		("0" + date.getDate()).slice(-2);
-};
-
-This.period = new This.Period();
-document.getElementById("periodStart").value = This.period.start;
-document.getElementById("periodEnd").value = This.period.end;
+exec("controls/period/init.js", This.afterInitPeriod);
 
