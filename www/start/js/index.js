82d837cd2176a173b163b5a3a26c06bb1ebc1ff1
aerix <vetalaerix@gmail.com>
2015-05-25 12:43:57 +0300

    Added onChange handler to dateStart and dateEnd.

--- www/start/js/index.js
+++ www/start/js/index.js
@@ -1,13 +1,34 @@
 This.afterInitPeriod = function()
 {
+	This.select();
+	
+	document.getElementById("periodStart").addEventListener
+	(
+		"change", 
+	 	This.period_onChange
+	);
+	document.getElementById("periodEnd").addEventListener
+	(
+		"change",
+		This.period_onChange
+	);
+};
+
+This.select = function()
+{
 	var periodStart = document.getElementById("periodStart").value;
 	var periodEnd = document.getElementById("periodEnd").value;
-	
+
 	call("dashboard_select", [periodStart, periodEnd], function(data)
-	{
-		document.getElementById("expenses_amount_total").innerHTML = data[0].expenses_amount_total;
-	});
+		 {
+			 document.getElementById("expenses_amount_total").innerHTML = data[0].expenses_amount_total;
+
+		 });
+};
+
+This.period_onChange = function()
+{
+	This.select();
 };
 
 exec("controls/period/init.js", This.afterInitPeriod);
-
